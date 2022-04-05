be1c75b7d3f218b5c792e09f9ed7150e4aedde86
attuspacheco <attuspacheco@gmail.com>
2022-04-05 00:36:09 -0300

    create form for create todo]

---
+++ lib/models/todo.dart
@@ -0,0 +1,8 @@
+class Todo {
+  String title;
+  String date;
+  String description;
+  int priority;
+
+  Todo({required this.title, required this.date, required this.description, required this.priority});
+}
